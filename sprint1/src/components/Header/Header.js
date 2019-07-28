2fcbfe00c00e69cb72d9cc3a27d1280a718be4d2
luyaozhou000 <luyaozhou@hotmail.com>
2019-07-28 03:46:10 -0400

    updated tablet site

--- sprint1/src/components/Header/Header.js
+++ sprint1/src/components/Header/Header.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import logo from '../../Assets/Logo/Logo-brainflix.svg'
-import './Header.scss'
-import Form from '../Form/Form'
+import React from "react";
+import logo from "../../Assets/Logo/Logo-brainflix.svg";
+import "./Header.scss";
+import searchLogo from "../../Assets/Icons/SVG/Icon-search.svg";
+import plusSign from "../../Assets/Icons/SVG/Icon-upload.svg";
+import mohan from "../../Assets/Images/Mohan-muruge.jpg";
 
 class Header extends React.Component {
-  render(){
-      return (
-          <>
-          <div className="Header-logo-container">
-             <img  src={logo} className="Header-logo" alt="logo" />
+  render() {
+    return (
+      // this head-wrapper div was inserted afterwards to use flex:
+      <div className="header-wrapper">
+        <div className="Header-logo-container">
+          <img src={logo} className="Header-logo" alt="logo" />
+        </div>
+
+        <form className="header-form">
+          <input className="header-form__input" type="text" placeholder="Search" />
+          <div className="header-form__wrapper">
+            <button className="header-form__wrapper--button" type="submit">
+              UPLOAD
+            </button>
+            <img className="header-form__wrapper--img" src={mohan} />
           </div>
-
-         <Form/>
-
-
-
-
-
-          </>
-      )
+        </form>
+      </div>
+    );
   }
-
-
 }
 
-
-
-
-
-
-
-
-
-
-export default Header
+export default Header;
--- sprint1/src/components/MainVideo/MainVideo.js
+++ sprint1/src/components/MainVideo/MainVideo.js
@@ -11,17 +11,27 @@ class MainVideo extends React.Component {
     console.log(this.props.mainVideo)
      return (
       <div className="MainVideo">
-         <video className="MainVideo__video" poster={this.props.mainVideo.image} src={this.props.mainVideo.video}></video> 
-         
-         
-         <div className="MainVideo__video-btnContainer">
-           
-          <img className="playBtn" src={videoPlayBtn}/>
-           <div className="progressBar"></div>
-           <img className="loaderBtn" src={fullscreenBtn}/>
-           <img className="volumeBtn" src={videoVolumeBtn}/>
 
-         </div>
+        <video className="MainVideo__video" poster={this.props.mainVideo.image} src={this.props.mainVideo.video}> </video> 
+         
+        <div className="MainVideo__video-btnContainer">
+         
+           <div className="playBtn-background">
+              <img className="playBtn" src={videoPlayBtn}/>
+           </div>
+
+           
+           <div className="progressBar"> </div>
+          
+
+           <div className="loader-volume-background">
+              <img className="loaderBtn" src={fullscreenBtn}/>
+          
+              <img className="volumeBtn" src={videoVolumeBtn}/>
+           </div>
+
+        </div> 
+    
       </div>
      )
    }
